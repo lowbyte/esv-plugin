1d6446cb251608470f3c5ff64c02eba247da10a5
Columcille <Columcille@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-07-15 13:02:58 +0000

    Adding 3.4.2
    git-svn-id: https://plugins.svn.wordpress.org/esv-plugin/tags/3.4.2@54872 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- esv_adminOptions.php
+++ esv_adminOptions.php
@@ -1,107 +1,48 @@
 <?php
 if (! function_exists('esv_options_subpanel')) {
 	function esv_options_subpanel() {
-		global $wpdb, $table_prefix, $ESV_Version;
+		global $wpdb;
 
-		$table_name = $table_prefix . "esv";
+		$table_name = $wpdb->prefix . "esv";
 
-		// Set all the default options, starting with creating
-		// the ESV passage table
-		if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
-			esv_install_table();
-
-			if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") == $table_name) {
-  		?>
-  		<div class="updated"><p><strong>ESV database table has been created.</strong></p></div>
-  		<?php
-			} else {
-  		?>
-  		<div class="updated"><p><strong>Unknown error trying to create ESV database table!</strong></p></div>
-  		<?php
-			}
-		}
-
-		// Versions are stored as strings. Reformat them so we can compare them.
-		// Starting with version 2.0.5, internal version numbers will always have
-		// at least three numbers, even if that means version numbers like
-		// 2.1.0 or 3.0.0
-		$oldvers = str_replace(".", "", get_option('esv_version'));
-		$curvers = str_replace(".", "", $ESV_Version);
-
-		// See if a 1.x version is installed
-		if (get_option('esv_audio_fmt') != "" && $oldvers == "")
+		if (isset($_GET['action']) && !isset($_POST['info_update']))
 		{
-			$oldvers = 100;
-		}
-
-		if ($oldvers < $curvers)
-		{
-			// Is this a new install? Load default data
-			if ($oldvers == "" || $oldvers == 0)
+			if ($_GET['action'] == "esv_purgedbase")
 			{
-				update_option('esv_include_reference', 'true');
-				update_option('esv_first_verse_num', 'true');
-				update_option('esv_verse_num', 'true');
-				update_option('esv_footnote', 'false');
-				update_option('esv_footnote_link', 'false');
-				update_option('esv_incl_headings', 'false');
-				update_option('esv_incl_subheadings', 'false');
-				update_option('esv_surround_chap', 'false');
-				update_option('esv_inc_audio', 'true');
-				update_option('esv_audio_fmt', 'real');
-				update_option('esv_incl_short_copyright', 'true');
-				update_option('esv_incl_copyright', 'false');
-				update_option('esv_ref_action', 'link');
-				update_option('esv_show_header', 'true');
-				update_option('esv_process_ref', 'runtime');
-				update_option('esv_backward_compat', 'false');
-				update_option('esv_parse_saved', 'false');
-			} else if ($oldvers < 310) {
-				update_option('esv_process_ref', 'runtime');
-				update_option('esv_backward_compat', 'false');
-				update_option('esv_parse_saved', 'false');
-			} else if ($oldvers < 210) {
-				update_option('esv_show_header', 'true');
-			}
-
-			update_option('esv_version', $ESV_Version);
-		}
-
-		if (isset($_GET['action']) && !isset($_POST['info_update'])) {
-			if ($_GET['action'] == "esv_purgedbase") {
-				if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
-      	  ?>
-	  	  <div class="updated"><p><strong>Purge Error: ESV database table not found.</strong></p></div>
-	 	  <?php
+				if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name)
+				{
+					echo '<div class="updated"><p><strong>Purge Error: ESV database table not found.</strong></p></div>';
 				} else {
 					$query = "DELETE FROM ". $table_name .";";
 					$wpdb->query($query);
-
-	  	  ?>
-     	  <div class="updated"><p><strong>Stored ESV passages have been cleared.</strong></p></div>
-	  	  <?php
+					
+					echo '<div class="updated"><p><strong>Stored ESV passages have been cleared.</strong></p></div>';
 				}
 			}
 		}
 
-		if (isset($_GET['action']) && !isset($_POST['info_update'])) {
-			if ($_GET['action'] == "esv_purgeall") {
+		if (isset($_GET['action']) && !isset($_POST['info_update']))
+		{
+			if ($_GET['action'] == "esv_purgeall")
+			{
 				if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
-      	  ?>
-	  	  <div class="updated"><p><strong>Purge Error: ESV database table not found.</strong></p></div>
-	 	  <?php
+					echo '<div class="updated"><p><strong>Purge Error: ESV database table not found.</strong></p></div>';
 				} else {
 					$query = "DROP TABLE ". $table_name .";";
 					$wpdb->query($query);
 
 					$query = "DELETE FROM wp_options WHERE option_name LIKE 'esv%';";
 					$wpdb->query($query);
-
-	  	  ?>
-     	  <div class="updated"><p><strong>All ESV Plugin data have been cleared. To re-load, visit the ESV Options page again.</strong></p></div>
-	  	  <?php
+					
+					echo '<div class="updated"><p><strong>All ESV Plugin data have been cleared. To re-load, visit the ESV Options page again.</strong></p></div>';
 				}
 			}
+		}
+		
+		if ($_GET['action'] != "esv_purgeall")
+		{
+			// Check all of our settings
+			esv_activate();
 		}
 
 		if (isset($_POST['info_update'])) {
@@ -160,9 +101,9 @@ if (! function_exists('esv_options_subpanel')) {
 			if ($_POST['esv_backward_compat'] == "") {
 				$_POST['esv_backward_compat'] = "false";
 			}
-
-			if ($_POST['esv_parse_saved'] == "") {
-				$_POST['esv_parse_saved'] = "false";
+            
+            if ($_POST['esv_incl_word_ids'] == "") {
+				$_POST['esv_incl_word_ids'] = "false";
 			}
 
 			update_option('esv_webkey', $_POST['esv_webkey']);
@@ -176,19 +117,18 @@ if (! function_exists('esv_options_subpanel')) {
 			update_option('esv_surround_chap', $_POST['esv_surround_chap']);
 			update_option('esv_inc_audio', $_POST['esv_inc_audio']);
 			update_option('esv_audio_fmt', $_POST['esv_audio_fmt']);
+            update_option('esv_audio_src', $_POST['esv_audio_src']);
 			update_option('esv_incl_short_copyright', $_POST['esv_incl_short_copyright']);
 			update_option('esv_incl_copyright', $_POST['esv_incl_copyright']);
 			update_option('esv_ref_action', $_POST['esv_ref_action']);
 			update_option('esv_show_header', $_POST['esv_show_header']);
 			update_option('esv_process_ref', $_POST['esv_process_ref']);
+            update_option('esv_incl_word_ids', $_POST['esv_incl_word_ids']);
 			update_option('esv_backward_compat', $_POST['esv_backward_compat']);
-			update_option('esv_parse_saved', $_POST['esv_parse_saved']);
-
-	      ?>
-		  <div class="updated"><p><strong>Your options have been updated.</strong></p></div>
-		  <?php
+			
+			echo '<div class="updated"><p><strong>Your options have been updated.</strong></p></div>';
 		}
-
+		
 ?>
 
 <style type="text/css">
@@ -227,7 +167,6 @@ div.clearOptions {
 	<div class="esvOptionSection">
 		Options are grouped into the following sections:<br /><br />
 		<div class="esvOptions">
-			<a href="#uptodate">Up-to-date check</a><br />
 			<a href="#WebKey">ESV Web Key</a><br />
 			<a href="#Retrieval">Passage Retrieval Options</a><br />
 			<a href="#Format">Format Option</a><br />
@@ -238,44 +177,10 @@ div.clearOptions {
 		<br />
 
 		There are several options for the plugin. You should be safe leaving everything at their defaults, though you will probably want to glance over the Passage Retrieval options and may want to change the default Format option.<br /><br />
-		Help for the options can be found at the <a href="http://www.musterion.net/wordpress-esv-plugin/wordpress-esv-plugin-options/">ESV Plugin Options</a> page.
+		Help for the options can be found at the <a href="http://www.musterion.net/wordpress-esv-plugin/wordpress-esv-plugin-options-33x/">ESV Plugin Options</a> page.
 	</div>
 
-	<span class="esvOptionLabel"><a name="uptodate"></a>Up to date check</span><br />
-	<div class="esvOptionSection">
-		<?php
-		$currentVersion = esv_check_version();
-
-		if ($currentVersion != $ESV_Version)
-		{
-			$curvers = str_replace(".", "", $currentVersion);
-	    	$thisvers = str_replace(".", "", $ESV_Version);
-
-			if ($curvers > $thisvers)
-			{
-				?>
-					<b>You are not up to date</b>. Latest version of the plugin is <?php echo $currentVersion; ?> and you have version <?php echo $ESV_Version; ?> installed. Visit the <a href="http://www.musterion.net/wordpress-esv-plugin/">WordPress ESV Plugin</a> page to download the update.<br /><br />
-				<?php
-			} else {
-				?>
-					<b>You are living the future!</b>. Latest version of the plugin is <?php echo $currentVersion; ?> and you have version <?php echo $ESV_Version; ?> installed. Thank you for testing this version of the plugin. Visit the <a href="http://www.musterion.net/wordpress-esv-plugin/">WordPress ESV Plugin</a> page to see plugin news.<br /><br />
-				<?php
-			}
-		} else if ($currentVersion == "Error") {
-				?>
-					<br />
-					Error retrieving current version number.<br /><br />
-				<?php
-		} else {
-				?>
-					<br />
-					Your version of the plugin is up to date.<br /><br />
-				<?php
-		}
-		?>
-	</div>
-
-	<span class="esvOptionLabel"><a name="WebKey"></a>ESV Web Key</span> <span style="padding-left: 8px;">(<a target="_blank" href="http://www.musterion.net/?page_id=478#webkey">Help</a>)</span><br />
+	<span class="esvOptionLabel"><a name="WebKey"></a>ESV Web Key</span> <span style="padding-left: 8px;">(<a target="_blank" href="http://www.musterion.net/wordpress-esv-plugin/wordpress-esv-plugin-options-33x/#webkey">Help</a>)</span><br />
 	<div class="esvOptionSection">
 		Visit the <a href="http://www.gnpcb.org/esv/share/services/">ESV Bible Web Service</a> to obtain a personal key.<br /><br />
 
@@ -284,7 +189,7 @@ div.clearOptions {
 		</div>
 	</div>
 
-	<span class="esvOptionLabel"><a name="Retrieval"></a>Passage Retrieval Options</span> <span style="padding-left: 8px;">(<a target="_blank" href="http://www.musterion.net/?page_id=478#retrieval">Help</a>)</span><br />
+	<span class="esvOptionLabel"><a name="Retrieval"></a>Passage Retrieval Options</span> <span style="padding-left: 8px;">(<a target="_blank" href="http://www.musterion.net/wordpress-esv-plugin/wordpress-esv-plugin-options-33x/#retrieval">Help</a>)</span><br />
 	<div class="esvOptionSection">
 	    Visit the <a href="http://www.gnpcb.org/esv/share/services/api/#html">ESV Web Service API</a> for more information about these options.<br /><br />
 
@@ -339,13 +244,28 @@ div.clearOptions {
 					include-copyright
 				</label><br />
 
+            <input id="esv_incl_word_ids" name="esv_incl_word_ids" type="checkbox" value="true" <?php if(get_option('esv_incl_word_ids') == "true") echo "checked" ?> />
+                <label for="esv_incl_word_ids" title="Includes an id with each word. See the API documentation.">
+                    include-word-ids
+                </label><br />
+
 			<br />
-			Audio settings: <br />
+			Audio format: <br />
 
 			<input id="esv_inc_audio" name="esv_inc_audio" type="checkbox" value="true" <?php if(get_option('esv_inc_audio') == "true") echo "checked" ?> />
 				<label for="esv_inc_audio" title="Display a link to play the audio for this passage">
 					include-audio-link
 				</label><br />
+
+            <input id="esv_audio_fmt_flash" name="esv_audio_fmt"  type="radio" value="flash" <?php if(get_option('esv_audio_fmt') == "flash") echo "checked" ?> />
+                <label for="esv_audio_fmt_flash" title="Use Flash format">
+                    Flash Format
+                </label>
+
+            <input id="esv_audio_fmt_mp3" name="esv_audio_fmt" type="radio" value="mp3" <?php if(get_option('esv_audio_fmt') == "mp3") echo "checked" ?> />
+                <label for="esv_audio_fmt_mp3" title="Use MP3 format">
+                    MP3 Format
+                </label>
 
 			<input id="esv_audio_fmt_real" name="esv_audio_fmt"  type="radio" value="real" <?php if(get_option('esv_audio_fmt') == "real") echo "checked" ?> />
 				<label for="esv_audio_fmt_real" title="Use Real Media format">
@@ -357,10 +277,29 @@ div.clearOptions {
 					WMA Format
 				</label><br />
 
+            <br />
+            Audio source: <br />
+            For this to work Audio Format must be set to Flash or MP3.<br />
+
+            <input id="esv_audio_src_mm" name="esv_audio_src" type="radio" value="mm" <?php if(get_option('esv_audio_src') == "mm") echo "checked" ?> />
+                <label for="esv_audio_src_mm" title="The Bible as read by MaxMcLean">
+                    Max McLean (Whole Bible)
+                </label><br />
+
+            <input id="esv_audio_src_ml" name="esv_audio_src" type="radio" value="ml" <?php if(get_option('esv_audio_src') == "ml") echo "checked" ?> />
+                <label for="esv_audio_src_ml" title="The New Testament as read by Marquis Laughlin">
+                    Marquis Laughlin (New Testament Only)
+                </label><br />
+
+            <input id="esv_audio_src_mm_ml" name="esv_audio_src" type="radio" value="ml-mm" <?php if(get_option('esv_audio_src') == "ml-mm") echo "checked" ?> />
+                <label for="esv_audio_src_mm_ml" title="The OT read by Max McLean, the NT read by Marquis Laughlin">
+                    Mixed (McLean OT, Laughlin NT)
+                </label><br />
+
 		</div>
 	</div>
 
-	<span class="esvOptionLabel"><a name="Format"></a>Format Option</span> <span style="padding-left: 8px;">(<a target="_blank" href="http://www.musterion.net/?page_id=478#reference">Help</a>)</span><br />
+	<span class="esvOptionLabel"><a name="Format"></a>Format Option</span> <span style="padding-left: 8px;">(<a target="_blank" href="http://www.musterion.net/wordpress-esv-plugin/wordpress-esv-plugin-options-33x/#reference">Help</a>)</span><br />
 	<div class="esvOptionSection">
 		<br />
 		<div class="esvOptions">
@@ -391,30 +330,25 @@ div.clearOptions {
 		</div>
 	</div>
 
-	<span class="esvOptionLabel">When to Process References</span> <span style="padding-left: 8px;">(<a target="_blank" href="http://www.musterion.net/?page_id=478#process">Help</a>)</span><br />
+	<span class="esvOptionLabel">When to Process References</span> <span style="padding-left: 8px;">(<a target="_blank" href="http://www.musterion.net/wordpress-esv-plugin/wordpress-esv-plugin-options-33x/#process">Help</a>)</span><br />
 	<div class="esvOptionSection">
-		You will likely want to read the <a href="http://www.musterion.net/?page_id=478#process">Help</a> information for these options.<br /><br />
+		You may want to read the <a href="http://www.musterion.net/wordpress-esv-plugin/wordpress-esv-plugin-options-33x/#process">Help</a> information for these options.<br /><br />
 
 		<div class="esvOptions">
 			<input id="esv_process_ref_1" name="esv_process_ref" type="radio" value="runtime" <?php if(get_option('esv_process_ref') == "runtime") echo "checked" ?> />
 				<label for="esv_process_ref_1" title="This will modify only what is sent to the visitor when they view your site">
-					Process references at runtime
-				</label>
-
-			<input style="margin-left: 25px;" id="esv_parse_saved" name="esv_parse_saved" type="checkbox" value="true" <?php if(get_option('esv_parse_saved') == "true") echo "checked" ?> />
-				<label for="esv_parse_saved" title="Search for and process Scripture references saved with Option Two">
-					Search for and process Scripture references saved with Option Two
+                    Dynamic: Find and format Bible passages when visitors come to your site
 				</label><br />
 
 			<input id="esv_process_ref_2" name="esv_process_ref" type="radio" value="save" <?php if(get_option('esv_process_ref') == "save") echo "checked" ?> />
 				<label for="esv_process_ref_2" title="This will modify your posts when you save them">
-					Process references when saving posts
+                    Static: Find and format Bible passages when saving the post
 				</label><br />
 
 		</div>
 	</div>
 
-	<span class="esvOptionLabel">Backward Compatibility</span> <span style="padding-left: 8px;">(<a target="_blank" href="http://www.musterion.net/?page_id=478#backward">Help</a>)</span><br />
+	<span class="esvOptionLabel">Backward Compatibility</span> <span style="padding-left: 8px;">(<a target="_blank" href="http://www.musterion.net/wordpress-esv-plugin/wordpress-esv-plugin-options-33x/#backward">Help</a>)</span><br />
 	<div class="esvOptionSection">
 		<br />
 		<div class="esvOptions">
@@ -425,7 +359,7 @@ div.clearOptions {
 		</div>
 	</div>
 
-    <span class="esvOptionLabel"><a name="Settings"></a>Manage Database/Settings</span> <span style="padding-left: 8px;">(<a target="_blank" href="http://www.musterion.net/?page_id=478#manage">Help</a>)</span><br />
+    <span class="esvOptionLabel"><a name="Settings"></a>Manage Database/Settings</span> <span style="padding-left: 8px;">(<a target="_blank" href="http://www.musterion.net/wordpress-esv-plugin/wordpress-esv-plugin-options-33x/#manage">Help</a>)</span><br />
 	<div class="esvOptionSection">
 		If you want to clear out all of your stored ESV passages, just click <a href="<?php echo get_bloginfo('wpurl'); ?>/wp-admin/options-general.php?page=esv.php&action=esv_purgedbase">clear stored passages</a>.<br /><br />
 
@@ -459,53 +393,5 @@ div.clearOptions {
 
 <?php
   }
-}
-
-if (! function_exists('esv_check_version')) {
-	function esv_check_version() {
-		$currentversion = 0;
-
-		$url = "http://www.musterion.net/plugins/esv_plugin.txt";
-
-		if (function_exists("curl_init")) {
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			$currentversion = curl_exec($ch);
-			curl_close($ch);
-		} else if (ini_get('allow_url_fopen') == true) {
-			if ($rvs = fopen($url, 'r')) {
-				$currentversion = "";
-
-				while (!feof($rvs)) {
-					$currentversion .= fgets($rvs);
-				}
-
-				fclose($rvs);
-			}
-		} else {
-			$currentversion = "Error";
-		}
-
-		return trim($currentversion);
-	}
-}
-
-if (! function_exists('esv_install_table')) {
-	function esv_install_table() {
-		global $table_prefix, $wpdb;
-
-		$table_name = $table_prefix . "esv";
-
-		$sql = "CREATE TABLE ". $table_name ." (
-              Reference tinytext,
-              Verse     blob,
-              Added     datetime
-            );";
-
-		require_once(ABSPATH . 'wp-admin/upgrade-functions.php');
-		dbDelta($sql);
-
-		update_option('esv_webkey', 'IP');
-	}
 }
 ?>
